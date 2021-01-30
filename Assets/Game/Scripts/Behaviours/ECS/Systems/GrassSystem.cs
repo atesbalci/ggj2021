2a2001d650416f88f0c8b994d970e42f50844014
Mustafa Ertugrul Erdogan <mustafaertugrulerdogan@gmail.com>
2021-01-31 00:52:10 +0300

    Refactored grass collision system

--- Assets/Game/Scripts/Behaviours/ECS/Systems/GrassSystem.cs
+++ Assets/Game/Scripts/Behaviours/ECS/Systems/GrassSystem.cs
@@ -26,21 +26,20 @@ namespace Game.Behaviours.ECS.Systems
             var collidersQuery = GetEntityQuery(typeof(AABB));
             
             var translations = entityQuery.ToComponentDataArray<Translation>(Allocator.TempJob);
-            var rotations = entityQuery.ToComponentDataArray<Rotation>(Allocator.TempJob);
             var colliders = collidersQuery.ToComponentDataArray<AABB>(Allocator.TempJob);
             
+            var rotations = new NativeArray<Rotation>(translations.Length, Allocator.TempJob);
             var collisionResults = new NativeArray<bool>(translations.Length, Allocator.TempJob);
-            var nextPositions = new NativeArray<float3>(translations.Length, (Allocator.TempJob));
-            var directions = new NativeArray<float3>(translations.Length, (Allocator.TempJob));
-            var distances = new NativeArray<float>(translations.Length, (Allocator.TempJob));
-            
+            var nextPositions = new NativeArray<float3>(translations.Length, Allocator.TempJob);
+
             var distanceCheckJob = new DistanceCheckJob
             {
                 PlayerLoc = playerLoc,
+                PlayerForward = playerForward,
+                PlayerRight = playerRight,
                 Translations = translations,
                 NextPositions = nextPositions,
-                Direction = directions,
-                Distances = distances,
+                Rotations = rotations,
             };
             
             var distanceCheckHandle = distanceCheckJob.Schedule(translations.Length, 32);
@@ -66,60 +65,60 @@ namespace Game.Behaviours.ECS.Systems
                         Value = nextPositions[i]
                     };
                     
-                    if (distances[i] < Constants.GrassCloseThresholdSq)
-                    {
-                        var rotation = new Rotation
-                        {
-                            Value = quaternion.AxisAngle(playerForward,
-                                (1f - distances[i] / Constants.GrassCloseThresholdSq) * (math.PI / 12f) *
-                                (math.dot(playerRight, -directions[i]) > 0 ? -1f : 1f)),
-                        };
-                        
-                        EntityManager.SetComponentData(entities[i], rotation);
-                    }
-                    else
-                    {
-                        var rotation = new Rotation
-                        {
-                            Value = quaternion.identity
-                        };
-                        
-                        EntityManager.SetComponentData(entities[i], rotation);
-                    }
-                
+                    EntityManager.SetComponentData(entities[i], rotations[i]);
                     EntityManager.SetComponentData(entities[i], translation);
                 }
             }
             
-            translations.Dispose();
-            rotations.Dispose();
-            distances.Dispose();
-            directions.Dispose();
             entities.Dispose();
+            translations.Dispose();
+            colliders.Dispose();
+            rotations.Dispose();
             collisionResults.Dispose();
             nextPositions.Dispose();
-            colliders.Dispose();
         }
 
         private struct DistanceCheckJob : IJobParallelFor
         {
             [ReadOnly] public float3 PlayerLoc;
+            [ReadOnly] public float3 PlayerForward;
+            [ReadOnly] public float3 PlayerRight;
             [ReadOnly] public NativeArray<Translation> Translations;
             public NativeArray<float3> NextPositions;
-            public NativeArray<float3> Direction;
-            public NativeArray<float> Distances;
+            public NativeArray<Rotation> Rotations;
             
             public void Execute(int i)
             {
-                Direction[i] = PlayerLoc - Translations[i].Value;
-                Distances[i] = math.lengthsq(Direction[i]);
-                if (Distances[i] > Constants.MaxDistSq)
+                var direction = PlayerLoc - Translations[i].Value;
+                var distance = math.lengthsq(direction);
+                if (distance > Constants.MaxDistSq)
                 {
-                    NextPositions[i] = PlayerLoc + math.normalize(Direction[i]) * Constants.MaxDist;
+                    NextPositions[i] = PlayerLoc + math.normalize(direction) * Constants.MaxDist;
                 }
                 else
                 {
                     NextPositions[i] = Translations[i].Value;
+                }
+                
+                if (distance < Constants.GrassCloseThresholdSq)
+                {
+                    var rotation = new Rotation
+                    {
+                        Value = quaternion.AxisAngle(PlayerForward,
+                            (1f - distance / Constants.GrassCloseThresholdSq) * (math.PI / 12f) *
+                            (math.dot(PlayerRight, -direction) > 0 ? -1f : 1f)),
+                    };
+                    
+                    Rotations[i] = rotation;
+                }
+                else
+                {
+                    var rotation = new Rotation
+                    {
+                        Value = quaternion.identity
+                    };
+                    
+                    Rotations[i] = rotation;
                 }
             }
         }
