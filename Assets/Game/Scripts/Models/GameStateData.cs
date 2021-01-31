bd770c9751fa64638f2d954e227bbb76f4882739
Huseyin Erdogan <erdoganhuse@gmail.com>
2021-01-31 11:01:28 +0300

    Added collectable storage persistence

--- Assets/Game/Scripts/Models/GameStateData.cs
+++ Assets/Game/Scripts/Models/GameStateData.cs
@@ -4,13 +4,24 @@ namespace Game.Models
 {
     public class GameStateData
     {
+        private int _pickedCollectableCount;
         private CheckPoint _lastReachedCheckpoint;
 
+        public void PickCollectable()
+        {
+            _pickedCollectableCount++;
+        }
+
+        public int GetPickedCollectableCount()
+        {
+            return _pickedCollectableCount;
+        }
+        
         public void ReachCheckPoint(int no, Vector3 spawnPoint)
         {
             _lastReachedCheckpoint = new CheckPoint(no, spawnPoint);
         }
-
+        
         public Vector3? LastCheckpoint =>
             _lastReachedCheckpoint.No > 0 ? _lastReachedCheckpoint.SpawnPoint : (Vector3?) null;
 
