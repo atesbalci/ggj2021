86a847affb194f7eeb4b783d8ea12522d274f6eb
Huseyin Erdogan <erdoganhuse@gmail.com>
2021-01-31 12:34:55 +0300

    Updated ai settings

--- Assets/Game/Scripts/Behaviours/Enemy/AI/States/CatchState.cs
+++ Assets/Game/Scripts/Behaviours/Enemy/AI/States/CatchState.cs
@@ -36,7 +36,7 @@ namespace Game.Behaviours.Enemy.AI.States
 			CharacterInputBehaviour characterInputBehaviour = _player.GetComponent<CharacterInputBehaviour>();
 			characterInputBehaviour.enabled = false;
 
-			Vector3 targetPosition = _player.position + _player.forward * 0.5f;
+			Vector3 targetPosition = _player.position + _player.forward * 0.625f;
 			targetPosition = new Vector3(targetPosition.x, 0f, targetPosition.z);
 			
 			float moveStartTime = Time.time;
